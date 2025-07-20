22c6e65c50d838ebd1c3dbb1005c39529787bd80
jlh5360 <jlh5360@rit.edu>
2025-07-20 18:23:15 -0400

    Updated the comments to be clean Javadocs for the public API for the Page and View Object components

--- src/test/java/edu/rit/swen253/page/wikipedia/WikipediaHomePage.java
+++ src/test/java/edu/rit/swen253/page/wikipedia/WikipediaHomePage.java
@@ -10,13 +10,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-//Page Object for the Wikipedia Home Page
+/**
+ * Page Object for the Wikipedia Home Page.
+ */
 public class WikipediaHomePage extends AbstractPage {
     private static final String WIKIPEDIA_URL = "https://www.wikipedia.org/";
     private static final By SEARCH_INPUT_FINDER = By.id("searchInput");
     private static final By SEARCH_BUTTON_FINDER = By.cssSelector("button[type='submit']");   //The search button has type submit
 
-    //Constructs a WikipediaHomePage object
+    /**
+     * Constructs a WikipediaHomePage object.
+     */
     public WikipediaHomePage() {
         super();
 
@@ -29,13 +33,19 @@ public class WikipediaHomePage extends AbstractPage {
         }
     }
 
-    //Navigates to the Wikipedia Home page
+    /**
+     * Navigates to the Wikipedia Home page.
+     * @return a new instance of WikipediaHomePage.
+     */
     public static WikipediaHomePage navigateTo() {
-        //Returns a new instance of WikipediaHomePage
         return SeleniumUtils.getInstance().navigateToPage(WIKIPEDIA_URL, WikipediaHomePage::new);
     }
     
-    //Enters a search phrase into the search input field and submits the search
+    /**
+     * Enters a search phrase into the search input field and submits the search.
+     * @param searchPhrase The phrase to search for.
+     * @return The WikipediaSearchResultsPage object.
+     */
     public WikipediaSearchResultsPage performSearch(String searchPhrase) {
         DomElement searchInput = findOnPage(SEARCH_INPUT_FINDER);   //Find the search input element after ensuring it's present
         searchInput.sendKeys(searchPhrase);   //Input the desired phrase to search
@@ -47,19 +57,22 @@ public class WikipediaHomePage extends AbstractPage {
         //org.openqa.selenium.StaleElementReferenceException: stale element reference: stale element not found
         SeleniumUtils.getInstance().getLongWait().until(ExpectedConditions.presenceOfElementLocated(WikipediaSearchResultsPage.SEARCH_RESULTS_LIST_FINDER));
 
-        //Returns the WikipediaSearchResultsPage object
         return new WikipediaSearchResultsPage();
     }
     
-    //Finds the search input element on the page
+    /**
+     * Finds the search input element on the page.
+     * @return The DomElement representing the search input.
+     */
     public DomElement getSearchInput() {
-        //Returns the DomElement representing the search input
         return findOnPage(SEARCH_INPUT_FINDER);
     }
 
-    //Finds the search button element on the page
+    /**
+     * Finds the search button element on the page.
+     * @return The DomElement representing the search button.
+     */
     public DomElement getSearchButton() {
-        //Returns the DomElement representing the search button
         return findOnPage(SEARCH_BUTTON_FINDER);
     }
 }
--- src/test/java/edu/rit/swen253/page/wikipedia/WikipediaSearchResultsPage.java
+++ src/test/java/edu/rit/swen253/page/wikipedia/WikipediaSearchResultsPage.java
@@ -11,12 +11,16 @@ import static org.junit.jupiter.api.Assertions.fail;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//Page Object for the Wikipedia Search Results Page
+/**
+ * Page Object for the Wikipedia Search Results Page.
+ */
 public class WikipediaSearchResultsPage extends AbstractPage {
     public static final By SEARCH_RESULTS_LIST_FINDER = By.cssSelector("ul.mw-search-results");
     private static final By SEARCH_RESULT_ITEM_FINDER = By.cssSelector("li.mw-search-result");
 
-    //Constructs a WikipediaSearchResultsPage object
+    /**
+     * Constructs a WikipediaSearchResultsPage object.
+     */
     public WikipediaSearchResultsPage() {
         super();
 
@@ -29,22 +33,25 @@ public class WikipediaSearchResultsPage extends AbstractPage {
         }
     }
 
-    //Gets a list of all search result view objects on the page
+    /**
+     * Gets a list of all search result view objects on the page.
+     * @return A list of WikipediaSearchResultView objects.
+     */
     public List<WikipediaSearchResultView> getSearchResults() {
         //Find the search results list element after ensuring it's present
         DomElement searchResultsList = findOnPage(SEARCH_RESULTS_LIST_FINDER);
 
-        //Get the search results list and collect all the individual result together
-        //Returns a list of WikipediaSearchResultView objects
         return searchResultsList.findChildrenBy(SEARCH_RESULT_ITEM_FINDER)
                 .stream()
                 .map(WikipediaSearchResultView::new)
                 .collect(Collectors.toList());
     }
     
-    //Checks if the search results are empty
+    /**
+     * Checks if the search results are empty.
+     * @return true if there are no search results, false otherwise.
+     */
     public boolean areSearchResultsEmpty() {
-        //Returns true if there are no search results, false otherwise
         return getSearchResults().isEmpty();
     }
 }
