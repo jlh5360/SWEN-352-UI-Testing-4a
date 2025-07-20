34dc79858d8ef39818d7f0a9d395ad833f276e67
jlh5360 <jlh5360@rit.edu>
2025-07-20 14:52:28 -0400

    Added a test to navigate to the Wikipedia home page, which successfully works/passes

--- src/test/java/edu/rit/swen253/test/wikipedia/WikipediaSearchTest.java
+++ src/test/java/edu/rit/swen253/test/wikipedia/WikipediaSearchTest.java
@@ -1,5 +1,27 @@
 package edu.rit.swen253.test.wikipedia;
 
-public class WikipediaSearchTest {
-    
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import edu.rit.swen253.page.wikipedia.WikipediaHomePage;
+import edu.rit.swen253.test.AbstractWebTest;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DisplayName("Wikipedia Search Test")
+public class WikipediaSearchTest extends AbstractWebTest {
+    private WikipediaHomePage wikipediaHomePage;
+
+    @Test
+    @Order(1)
+    @DisplayName("Navigate to Wikipedia Home Page")
+    void navigateToHomePage() {
+        wikipediaHomePage = WikipediaHomePage.navigateTo();
+        
+        assertNotNull(wikipediaHomePage, "Wikipedia Home Page should be loaded.");
+    }
 }
