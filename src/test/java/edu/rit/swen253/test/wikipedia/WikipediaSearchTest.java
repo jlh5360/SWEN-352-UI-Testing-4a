6fc206df547d4e8c917924fc86b27da94056e139
jlh5360 <jlh5360@rit.edu>
2025-07-20 14:59:40 -0400

    Added a logger to make things clearer in the terminal (and for future implementation --> to display the title and URL for each search result)

--- src/test/java/edu/rit/swen253/test/wikipedia/WikipediaSearchTest.java
+++ src/test/java/edu/rit/swen253/test/wikipedia/WikipediaSearchTest.java
@@ -2,6 +2,8 @@ package edu.rit.swen253.test.wikipedia;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.logging.Logger;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -14,14 +16,19 @@ import edu.rit.swen253.test.AbstractWebTest;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DisplayName("Wikipedia Search Test")
 public class WikipediaSearchTest extends AbstractWebTest {
+    private static final Logger logger = Logger.getLogger(WikipediaSearchTest.class.getName());
+
     private WikipediaHomePage wikipediaHomePage;
 
     @Test
     @Order(1)
     @DisplayName("Navigate to Wikipedia Home Page")
     void navigateToHomePage() {
+        logger.info("Navigating to Wikipedia Home Page.");
         wikipediaHomePage = WikipediaHomePage.navigateTo();
-        
+
         assertNotNull(wikipediaHomePage, "Wikipedia Home Page should be loaded.");
+        
+        logger.info("Successfully navigated to Wikipedia Home Page.");
     }
 }
